1ec0820734a5af9437f52a464ec99cdc614c921a
mayankav <mayankmacav@gmail.com>
2023-12-02 00:51:03 +0400

    feat: saveData locally

---
+++ src/popup/helper/saveDataLocally.ts
@@ -0,0 +1,21 @@
+import { TranscriptRecord } from "../components/Transcript";
+
+export function saveDataLocally(
+  key: string,
+  transcriptData: Array<TranscriptRecord>,
+  commentsData: Array<any>
+) {
+  const dataToSave = {
+    transcriptData,
+    commentsData,
+  };
+
+  chrome.storage.local.set(
+    {
+      key: dataToSave,
+    },
+    () => {
+      console.log("Data saved locally:", dataToSave);
+    }
+  );
+}
