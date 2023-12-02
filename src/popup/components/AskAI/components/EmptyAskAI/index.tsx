5331364b81aeb1045039f80d648845d4209c7d52
mayankav <mayankmacav@gmail.com>
2023-12-02 20:57:55 +0400

    fix: styling askai

--- src/popup/components/AskAI/components/EmptyAskAI/index.tsx
+++ src/popup/components/AskAI/components/EmptyAskAI/index.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import Button from "../../../ui-components/button";
-import { AskMeIcon, AskMeIconBig } from "../../../../icons";
+import { AskMeIconBig } from "../../../../icons";
 import CircleIcon from "../../../ui-components/CircleIcon";
 import "./index.scss";
 
-interface EmptyAskAiProps {
-  generateAnswer: () => void;
-}
-const EmptyAskAI = ({ generateAnswer }: EmptyAskAiProps) => {
+const EmptyAskAI = () => {
   return (
     <div className="empty-askai-wrapper">
       <div className="icon-wrapper">
@@ -17,9 +13,6 @@ const EmptyAskAI = ({ generateAnswer }: EmptyAskAiProps) => {
         <h3>Ask AI?</h3>
         <p>Instantly uncover video insights. Generate intelligent summaries</p>
       </div>
-      <Button iconRight={<AskMeIcon />} onClick={generateAnswer}>
-        Generate
-      </Button>
     </div>
   );
 };
--- src/popup/components/AskAI/index.tsx
+++ src/popup/components/AskAI/index.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import EmptyAskAI from "./components/EmptyAskAI";
+import Button from "../ui-components/button";
+import { AskMeIconWhite, AskMeIconBig } from "../../icons";
+import "./index.scss";
 
 const AskAI = () => {
+  const [promptText, setPromptText] = useState("");
+  const generateAnswer = () => {
+    console.log(promptText);
+  };
+  const handlePromptChange = (e) => {
+    setPromptText(e.target.value);
+  };
   return (
-    <div>
-      <EmptyAskAI generateAnswer={() => {}} />
+    <div className="askai-wrapper">
+      <EmptyAskAI />
+      <textarea onChange={handlePromptChange} />
+      <Button iconRight={<AskMeIconWhite />} onClick={generateAnswer}>
+        Generate
+      </Button>
     </div>
   );
 };
