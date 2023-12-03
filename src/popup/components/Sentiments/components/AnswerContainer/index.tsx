117006390b80f737ed39a072d26bddeb86bd74cd
mayankav <mayankmacav@gmail.com>
2023-12-03 05:47:18 +0400

    feat: summary - incomplete

--- src/popup/components/Sentiments/components/AnswerContainer/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.scss";
-
-const AnsweContainer = ({ ansText }: { ansText: string }) => {
-  return <div className="ans-container">{ansText}</div>;
-};
-
-export default AnsweContainer;
--- src/popup/components/Sentiments/index.tsx
+++ src/popup/components/Sentiments/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import EmotionRow from "./components/EmotionRow";
 import "./index.scss";
 import Button from "../ui-components/button";
-import { AskMeIcon, SentimentIcon, SentimentIconBig } from "../../icons";
+import { AskMeIcon } from "../../icons";
 import EmptySentimentScreen from "./components/EmptySentimentScreen";
 import { sendMessageToBgScript } from "../../helper/sendMessageToBgScript";
 import { sendMessageToContentScript } from "../../helper/sendMessageToContentScript";
@@ -17,14 +17,11 @@ import {
   MessageToPopupTypeEnum,
 } from "../../../types";
 import { checkSentimentsLocally } from "../../../background/helper/checkSentimentsLocally";
-import { checkSummaryLocally } from "../../../background/helper/checkSummaryLocally";
-import AnsweContainer from "./components/AnswerContainer";
 
 const Sentiments = () => {
   // temporary state to toggle between empty and filled sentiment screen
   const [sentiments, setSentiments] = useState(false);
   const [sentimentsData, setSentimentsData] = useState<Sentiments>(null);
-  const [videoSummary, setVideoSummary] = useState<string>();
   const onAnalyseClickHandler = () => {
     setSentiments(!sentiments);
     const message: MessageToBgScript = {
@@ -34,39 +31,6 @@ const Sentiments = () => {
     };
     sendMessageToBgScript(message, (response) => {});
   };
-  const summarizeHandler = () => {
-    // video summary
-    const message: MessageToBgScript = {
-      action: MessageToBgScriptTypeEnum.CALL_AN_API,
-      apiName: ApiNamesEnum.getSummary,
-      source: ApiSourceEnum.video,
-    };
-    sendMessageToBgScript(message, (response) => {});
-  };
-
-  // useEffect(() => {
-  //   let interval;
-  //   if (sentimentsData === null) {
-  //     const message: MessageToContentScript = {
-  //       messageType: MessageToContentScriptTypeEnum.GET_UNIQUE_VIDEO_ID,
-  //     };
-  //     sendMessageToContentScript(message, (response) => {
-  //       const videoId = response.uniqueVideoId;
-  //       if (videoId) {
-  //         interval = setInterval(() => {
-  //           checkSentimentsLocally(videoId, (data) => {
-  //             setSentimentsData(data);
-  //           });
-  //         }, 500);
-  //       }
-  //     });
-  //   }
-  //   return () => {
-  //     clearInterval(interval);
-  //   };
-  // }, [sentimentsData]);
-
-  // listening to summary
 
   useEffect(() => {
     chrome.runtime.onMessage.addListener(
@@ -86,18 +50,10 @@ const Sentiments = () => {
             const videoId = response.uniqueVideoId;
             console.log("Message received in popup: videoId", videoId);
             if (videoId) {
-              if (property === "summary") {
-                checkSummaryLocally(videoId, "video", (data) => {
-                  console.log("PROPS IS summary", data);
-                  setVideoSummary(data.summary);
-                  setSentiments(false);
-                  setSentimentsData(null);
-                });
-              } else if (property === "sentiments") {
+              if (property === "sentiments") {
                 checkSentimentsLocally(videoId, (data) => {
                   console.log("PROPS IS sentiments", data);
                   setSentimentsData(data);
-                  setVideoSummary(null);
                 });
               }
             }
@@ -124,23 +80,9 @@ const Sentiments = () => {
           variant="secondary"
           fullWidth
           iconRight={<AskMeIcon />}
-          onClick={summarizeHandler}
+          onClick={() => {}}
         >
-          Summarize with AI
-        </Button>
-      </footer>
-    </>
-  ) : videoSummary ? (
-    <>
-      <AnsweContainer ansText={videoSummary} />
-      <footer>
-        <Button
-          variant="secondary"
-          fullWidth
-          iconRight={<SentimentIcon />}
-          onClick={onAnalyseClickHandler}
-        >
-          Analyze sentiment
+          Try more with AI
         </Button>
       </footer>
     </>
