3f8b6e63be124fe79a5d9be4c409488799861d17
mayankav <mayankmacav@gmail.com>
2023-12-03 06:31:23 +0400

    fix: image ans clickable

--- src/popup/components/AskAI/components/ImageAnswer/index.tsx
+++ src/popup/components/AskAI/components/ImageAnswer/index.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "./index.scss";
+import {
+  MessageToContentScript,
+  MessageToContentScriptTypeEnum,
+} from "../../../../../types";
+import { sendMessageToContentScript } from "../../../../helper/sendMessageToContentScript";
 
 interface ImageAnswerProps {
   time: number;
   answer: string;
 }
 const ImageAnswer = ({ time, answer }: ImageAnswerProps) => {
+  const timeClickHandler = () => {
+    const changeVideoTime: MessageToContentScript = {
+      messageType: MessageToContentScriptTypeEnum.MOVE_VIDEO_TO_TIME,
+      timestampInSeconds: time,
+    };
+    sendMessageToContentScript(changeVideoTime, () => {});
+  };
   return (
-    <div className="image-ans-wrapper">
+    <div className="image-ans-wrapper" onClick={timeClickHandler}>
       <div className="image-time">{time}</div>
       <div className="image-answer">{answer}</div>
     </div>
